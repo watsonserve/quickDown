4ddedf4a519faca8c3524e1b8eb98625d31b2cc0
james <hamishwatson@live.com>
2016-06-01 13:27:03 +0800

    update

--- src/quickDown/quickDown.go
+++ src/quickDown/quickDown.go
@@ -5,7 +5,8 @@ import (
     "fmt"
     "os"
     "net/url"
-    "quickDown/downloaders"
+    "net/http"
+    "io/ioutil"
 )
 
 func help() {
@@ -18,41 +19,47 @@ func help() {
     fmt.Fprintln(os.Stderr, "     -h show this help information\n")
 }
 
-func express(file *os.File, downer downloaders.Downloader, pipe chan [2]int64, repipe chan int64, id int) {
+func express(file *os.File, urlStr string, pipe chan [2]int64, repipe chan int64, id int) {
     for {
         task := <- pipe
         start := task[0]
         end := task[1]
-        httpRange := make(map[string]string)
-        httpRange["range"] = strconv.FormatInt(start, 10) + "-" + strconv.FormatInt(end, 10)
-        httpRange["method"] = "GET"
-        response, err := downer.Load(nil, &httpRange)
+
+        client := &http.Client{}
+        req, err := http.NewRequest("GET", urlStr, nil)
         if nil != err {
+            fmt.Fprintln(os.Stderr, err)
             return
         }
-        size, err := strconv.ParseInt((*response)["Content-Length"], 0, 0)
-        var length int64
-        fmt.Fprintf(os.Stderr, "id %d have load %d byte.\n", id, size)
-        length = 0
-        content := []byte((*response)["content"])
-        for length < size {
-
-            fmt.Fprintf(os.Stderr, "write\n")
-            leng, err := file.WriteAt(content[length:], start + length)
-            if nil != err {
-              fmt.Fprintln(os.Stderr, err)
-                return
-            }
-            length += int64(leng)
-            fmt.Fprintf(os.Stderr, "write length: %d\n", length)
+        req.Header.Add("range", strconv.FormatInt(start, 10) + "-" + strconv.FormatInt(end, 10))
+        resp, err := client.Do(req)
+        if nil != err {
+            fmt.Fprintln(os.Stderr, err)
+            return
         }
-        repipe <- length
+        if 200 == resp.StatusCode && 0 != id {
+            break
+        }
+        body, err := ioutil.ReadAll(resp.Body)
+        if nil != err {
+            fmt.Fprintln(os.Stderr, err)
+            return
+        }
+        resp.Body.Close()
+        leng, err := file.WriteAt(body, start)
+        if nil != err {
+            fmt.Fprintln(os.Stderr, err)
+            return
+        }
+        repipe <- int64(leng)
     }
+    return
 }
-func sendTask(block int64, allsize int64, taskPipe chan [2]int64) {
+
+func sendTask(block int64, allSize int64, taskPipe chan [2]int64) {
     var taskBlock [2]int64
     var off int64
-    for off = 0; off < allsize; {
+    for off = 0; off < allSize; {
         taskBlock[0] = off
         off += block
         taskBlock[1] = off
@@ -62,20 +69,21 @@ func sendTask(block int64, allsize int64, taskPipe chan [2]int64) {
     return
 }
 
-func ByHttp(outFile string, uri *url.URL, block *int64, sgmTrd *int64, notifyPipe chan int64) (int64, *os.File, error) {
-    downloader := downloaders.NewHttpDownloader(uri)
-    response, err := downloader.Load(nil, nil)
+func ByHttp(outFile string, uri string, block *int64, sgmTrd *int64, notifyPipe chan int64) (int64, *os.File, error) {
+    client := &http.Client{}
+    resp, err := client.Head(uri)
     if nil != err {
         return -1, nil, err
     }
-    allsize, err := strconv.ParseInt((*response)["Content-Length"], 0, 0)
+    resp.Body.Close()
+    allSize := resp.ContentLength
     if 1 == *sgmTrd {    // signal thread
-        *block = allsize
+        *block = allSize
     } else if 0 == *block {    // no repeat
-        *block = allsize / *sgmTrd +1
-    } else if 0 == *sgmTrd || allsize / *block < *sgmTrd {    // less thread or more thread
-        *sgmTrd = allsize / *block
-        if 0 != allsize % *block {
+        *block = allSize / *sgmTrd +1
+    } else if 0 == *sgmTrd || allSize / *block < *sgmTrd {    // less thread or more thread
+        *sgmTrd = allSize / *block
+        if 0 != allSize % *block {
             *sgmTrd++
         }
     }
@@ -86,16 +94,16 @@ func ByHttp(outFile string, uri *url.URL, block *int64, sgmTrd *int64, notifyPip
       return -1, nil, err
     }
     for i := 0; i < int(*sgmTrd); i++ {
-        go express(storer, downloader, taskPipe, notifyPipe, i)
+        go express(storer, uri, taskPipe, notifyPipe, i)
     }
-    go sendTask(*block, allsize, taskPipe)
-    return allsize, storer, nil
+    go sendTask(*block, allSize, taskPipe)
+    return allSize, storer, nil
 }
 
 func main() {
     argv := os.Args
     argc := len(argv)
-    if 2 > argc {
+    if 2 > argc {  // 没有给出任何参数
         help()
         return
     }
@@ -104,44 +112,51 @@ func main() {
     var sgmTrd int64
     var err error
     var uri *url.URL
-    var allsize int64
+    var allSize int64
     var storer *os.File
     block = 1
     outFile := ""
     // cfgFile := -1
     sgmTrd = 1
-    urlstr := ""
+    urlStr := ""
     // get option
-    for i := 2; i < argc; i++ {
-        if '-' == argv[i-1][0] {
-            switch(argv[i-1][1]) {
+    for i := 1; i < argc; i++ {
+        argp := argv[i]
+        var nextArg string
+        nextArg = ""
+        if i+1 < argc {
+            nextArg = argv[i+1]
+        }
+        if '-' == argp[0] {  // 一个选项
+            switch(argp[1]) {
                 case 'b':
-                    block, err = strconv.ParseInt(argv[i], 0, 0)
+                    block, err = strconv.ParseInt(nextArg, 0, 0)
                     if nil != err {
                         fmt.Fprintln(os.Stderr, "ERROR block should be a intger")
                         return
                     }
                     continue
                 case 'o':
-                    outFile = argv[i]
+                    outFile = nextArg
                     continue
                 case 't':
-                    sgmTrd, err = strconv.ParseInt(argv[i], 0, 0)
+                    sgmTrd, err = strconv.ParseInt(nextArg, 0, 0)
                     if nil != err {
                         fmt.Fprintln(os.Stderr, "ERROR number of thread should be a intger")
                         return
                     }
                     continue
                 default:
-                    fmt.Fprintln(os.Stderr, "ERROR unknow option " + argv[i])
+                    fmt.Fprintln(os.Stderr, "ERROR unknow option " + nextArg)
                 case 'h':
                     help()
                     return
             }
         } else {
-            urlstr = argv[i]
+            urlStr = argp  // 预下载文件地址
         }
     }
+
     if 0 == block && 0 == sgmTrd {
         fmt.Fprintln(os.Stderr, "ERROR block and number of thread can't both be 0")
         return
@@ -150,13 +165,14 @@ func main() {
     if sgmTrd > 512 {    // max number of thread
         sgmTrd = 512
     }
-    if "" == urlstr {
+    if "" == urlStr {
         help()
         return
     }
-    fmt.Fprintln(os.Stderr, "url: " + urlstr)
+    // debug
+    fmt.Fprintln(os.Stderr, "url: " + urlStr)
 
-    uri, err = url.Parse(urlstr)
+    uri, err = url.Parse(urlStr)
     if nil != err {
         fmt.Fprintln(os.Stderr, "ERROR url")
         return
@@ -168,21 +184,23 @@ func main() {
             fmt.Fprintln(os.Stderr, "parse base64")
             return
         case  "http":
-            allsize, storer, err = ByHttp(outFile, uri, &block, &sgmTrd, notifyPipe)
+            allSize, storer, err = ByHttp(outFile, urlStr, &block, &sgmTrd, notifyPipe)
             break
         default:
             fmt.Printf("ERROR unsuppored protocol %s\n", uri.Scheme)
             return
     }
-    fmt.Fprintf(os.Stderr, "file-size: %d\nwaiting...", allsize)
+
+
+    fmt.Fprintf(os.Stderr, "file-size: %d waiting...\n", allSize)
     for {
         finish := <- notifyPipe
         if -1 == finish {
             fmt.Fprintln(os.Stderr, "ERROR")
             return
         }
-        allsize -= finish
-        if 0 >= allsize {
+        allSize -= finish
+        if 0 >= allSize {
             break
         }
     }
